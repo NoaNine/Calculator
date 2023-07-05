f0b9eef7f3ef20dcd471829a22ddf02ca8b66050
Oleksandr <havlytskyi@gmail.com>
2023-07-05 12:15:37 +0300

    Added test for checking current culture decimal separator

--- CalculatorTest/MathTest.cs
+++ CalculatorTest/MathTest.cs
@@ -5,7 +5,8 @@ namespace CalculatorTest
     {
         private readonly Mock<IAnalyzator> _analyzatorMock = new Mock<IAnalyzator>();
         private readonly Mock<ICultureSettings> _cultureSettingsMock = new Mock<ICultureSettings>();
-        private readonly CultureInfo cultureInfo = new CultureInfo("en-US");
+        private readonly CultureInfo Culture = CultureInfo.CurrentCulture;
+        private readonly CultureInfo newCulture = new CultureInfo("en-US");
 
         [DataTestMethod]
         [DynamicData(nameof(MathTestData.Calculate_SimpleExpression_Data), typeof(MathTestData), DynamicDataSourceType.Method)]
@@ -41,34 +42,65 @@ namespace CalculatorTest
         [DynamicData(nameof(MathTestData.Calculate_BigDecimalNumber_Data), typeof(MathTestData), DynamicDataSourceType.Method)]
         public void Calculate_BigDecimalNumber(string input, List<Lexeme> output, double expected)
         {
-            CultureInfo.CurrentCulture = cultureInfo;
-            _cultureSettingsMock.SetupGet(x => x.Culture).Returns(cultureInfo);
-            _analyzatorMock.Setup(c => c.LexicalAnalyze(input)).Returns(output);
-            var math = new Calculator.Math(_analyzatorMock.Object, _cultureSettingsMock.Object);
-            var result = math.Calculate(input);
-            Assert.AreEqual(expected, result);
+            try
+            {
+                CultureInfo.CurrentCulture = newCulture;
+                _cultureSettingsMock.SetupGet(x => x.Culture).Returns(newCulture);
+                _analyzatorMock.Setup(c => c.LexicalAnalyze(input)).Returns(output);
+                var math = new Calculator.Math(_analyzatorMock.Object, _cultureSettingsMock.Object);
+                var result = math.Calculate(input);
+                Assert.AreEqual(expected, result);
+            }
+            finally 
+            {
+                CultureInfo.CurrentCulture = Culture;
+            }
         }
 
         [DataTestMethod]
         [DynamicData(nameof(MathTestData.Calculate_DecimalNumber_Data), typeof(MathTestData), DynamicDataSourceType.Method)]
         public void Calculate_DecimalNumber(string input, List<Lexeme> output, double expected)
         {
-            CultureInfo.CurrentCulture = cultureInfo;
-            _cultureSettingsMock.SetupGet(x => x.Culture).Returns(cultureInfo);
-            _analyzatorMock.Setup(c => c.LexicalAnalyze(input)).Returns(output);
-            var math = new Calculator.Math(_analyzatorMock.Object, _cultureSettingsMock.Object);
-            var result = math.Calculate(input);
-            Assert.AreEqual(expected, result);
+            try
+            {
+                CultureInfo.CurrentCulture = newCulture;
+                _cultureSettingsMock.SetupGet(x => x.Culture).Returns(newCulture);
+                _analyzatorMock.Setup(c => c.LexicalAnalyze(input)).Returns(output);
+                var math = new Calculator.Math(_analyzatorMock.Object, _cultureSettingsMock.Object);
+                var result = math.Calculate(input);
+                Assert.AreEqual(expected, result);
+            }
+            finally 
+            {
+                CultureInfo.CurrentCulture = Culture;
+            }
         }
 
         [DataTestMethod]
         [DynamicData(nameof(MathTestData.Calculate_LargeNumber_Data), typeof(MathTestData), DynamicDataSourceType.Method)]
         public void Calculate_LargeNumber(string input, List<Lexeme> output, double expected)
         {
-            CultureInfo.CurrentCulture = cultureInfo;
-            _cultureSettingsMock.SetupGet(x => x.Culture).Returns(cultureInfo);
+            try
+            {
+                CultureInfo.CurrentCulture = newCulture;
+                _cultureSettingsMock.SetupGet(x => x.Culture).Returns(newCulture);
+                _analyzatorMock.Setup(c => c.LexicalAnalyze(input)).Returns(output);
+                var math = new Calculator.Math(_analyzatorMock.Object, _cultureSettingsMock.Object);
+                var result = math.Calculate(input);
+                Assert.AreEqual(expected, result);
+            }
+            finally
+            {
+                CultureInfo.CurrentCulture = Culture;
+            }
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(MathTestData.Calculate_CheckCurrentCultureDecimalSeparator_Data), typeof(MathTestData), DynamicDataSourceType.Method)]
+        public void Calculate_CheckCurrentCultureDecimalSeparator(string input, List<Lexeme> output, double expected)
+        {
             _analyzatorMock.Setup(c => c.LexicalAnalyze(input)).Returns(output);
-            var math = new Calculator.Math(_analyzatorMock.Object, _cultureSettingsMock.Object);
+            var math = new Calculator.Math(_analyzatorMock.Object);
             var result = math.Calculate(input);
             Assert.AreEqual(expected, result);
         }
